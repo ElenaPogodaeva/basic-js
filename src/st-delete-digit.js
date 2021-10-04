485383ce92a467218e1291a24d6c8b8cd4bb37db
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2021-10-04 17:57:54 +0300

    feat: implement the function deleteDigit

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+  let digits = [];
+  while (n !== 0) {
+     let digit = n % 10;
+     digits.push(digit);
+     n = Math.floor(n / 10);
+  }
+  let result = 0;
+  for (let i = 0; i < digits.length; i++) {
+    let n = 0;
+    for (let j =digits.length - 1; j>=0; j-- ) {
+      if (i !== j) {
+        n = n*10 + digits[j];
+      }
+    }
+    result = Math.max(n, result);
+  }
+  return result;
 }
