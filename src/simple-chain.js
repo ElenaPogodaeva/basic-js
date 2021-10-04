6daec2db77e96c143cc5302bcd1fa406ecdbc628
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2021-10-04 17:52:21 +0300

    feat: implement the object chainMaker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,31 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
+  
+  array: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.array.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if ((typeof position !== 'number')||(position > this.array.length) ||(position < 1)) {
+     this.array.length = 0;
+      throw new Error('You can\'t remove incorrect link!');
+    }
+    this.array.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.array.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let arr = this.array.join('~~');
+    this.array.length = 0;
+    return arr;
   }
+ 
 };
