1698e652a54518af05ca5f5783a9f7613c42a394
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2021-10-04 17:56:49 +0300

    feat: implement the function getSeason

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,63 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  if (arguments.length === 0) {
+    return ('Unable to determine the time of year!');
+  } 
+  
+  if (date instanceof Date) {
+    if (isNaN(Date.parse(date))) 
+    {
+      throw new Error('Invalid date!');
+    }
+    if (Object.prototype.toString.call(date) !== '[object Date]') {
+      throw new Error('Invalid date!');
+    }
+  /* if (isNaN(date.getTime())) {
+    throw new Error('Invalid date!');
+   }*/
+  let month = date.getMonth();
+  let season;
+  switch (month) {
+    case 0: 
+      season = 'winter';
+      break;
+    case 1: 
+      season = 'winter';
+      break;
+    case 2: 
+      season = 'spring';
+      break;
+    case 3: 
+      season = 'spring';
+      break;
+    case 4: 
+      season = 'spring';
+      break;
+    case 5: 
+      season = 'summer';
+      break;
+    case 6: 
+      season = 'summer';
+      break;
+    case 7: 
+      season = 'summer';
+      break;
+    case 8: 
+      season = 'autumn';
+      break;
+    case 9: 
+      season = 'autumn';
+      break;
+    case 10: 
+      season = 'autumn';
+      break;
+    case 11: 
+      season = 'winter';
+      break;
+  }
+  return season;
+ }
+ else throw new Error('Invalid date!');
 }
